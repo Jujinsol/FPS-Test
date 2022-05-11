63238cd98766b218f9599efc2d6b0975a9fd55d0
Jujinsol <88575150+Jujinsol@users.noreply.github.com>
2022-05-11 17:10:37 +0900

    gunshot 수정

--- Assets/Script/Contents/GunShot.cs
+++ Assets/Script/Contents/GunShot.cs
@@ -32,6 +32,6 @@ public class GunShot : MonoBehaviour
         GameObject clone = GameObject.Find("Fire(Clone)");
         if (clone == null)
             return;
-        Destroy(clone, 0.2f);
+        Destroy(clone);
     }
 }
